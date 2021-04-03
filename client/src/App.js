1f538746aa382a889b15a8ee1891ff41d6d74011
Jeremy Parker <jrobinparker@gmail.com>
2021-04-03 12:26:08 -0700

    Added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Navbar from './components/navbar';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>Sage</h1>
     </div>
   );
---
+++ client/src/components/navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './navbar.css';
+
+function Navbar() {
+  return (
+    <nav>
+      <h3>Sage</h3>
+    </nav>
+  )
+}
+
+export default Navbar;
